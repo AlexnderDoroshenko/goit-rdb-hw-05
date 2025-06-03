df0eed18a08064f7552b2467088667bdb8c01c29
Alexander <AlexnderDoroshenko@users.noreply.github.com>
2025-06-03 21:10:30 +0300

    Update and rename subqueries_1-4.sql to queries_1-5.sql

--- queries_1-5.sql
+++ queries_1-5.sql
@@ -6,6 +6,7 @@ SELECT
      FROM orders o 
      WHERE o.order_id = od.order_id) AS customer_id
 FROM order_details od;
+
 // 2. Write an SQL query that will display the order_details table. Filter the results so that the corresponding record from the orders table satisfies the condition shipper_id=3.
 // This should be done using a nested query in the WHERE statement.
 SELECT * 
@@ -15,6 +16,7 @@ WHERE od.order_id IN (
     FROM orders o 
     WHERE o.shipper_id = 3
 );
+
 // 3. Write an SQL query, nested in the FROM statement, that will select rows with the condition quantity>10 from the order_details table. For the obtained data, find the average value of the quantity field — group by order_id.
 SELECT 
     filtered.order_id, 
@@ -25,6 +27,7 @@ FROM (
     WHERE quantity > 10
 ) AS filtered
 GROUP BY filtered.order_id;
+
 // 4. Solve problem 3 using the WITH statement to create a temporary table temp. If your MySQL version is earlier than 8.0, create this query by analogy with how it is done in the abstract.
 WITH temp AS (
     SELECT * 
@@ -36,3 +39,12 @@ SELECT
     AVG(temp.quantity) AS avg_quantity
 FROM temp
 GROUP BY temp.order_id;
+
+// 5. Create a function with two parameters that will divide the first parameter by the second. Both parameters and the return value must be of type FLOAT.
+// Use the DROP FUNCTION IF EXISTS construct. Apply the function to the quantity attribute of the order_details table. The second parameter can be any number you want.
+SELECT 
+    order_id,
+    product_id,
+    quantity,
+    divide_floats(quantity, 2.5) AS divided_quantity
+FROM order_details;
