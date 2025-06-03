69a9c81c5dca587b6d6094936a2fa4a78cdbcda9
Alexander <AlexnderDoroshenko@users.noreply.github.com>
2025-06-03 21:08:09 +0300

    Create divide_floats_func.sql

---
+++ divide_floats_func.sql
@@ -0,0 +1,17 @@
+DROP FUNCTION IF EXISTS divide_floats;
+
+DELIMITER //
+
+CREATE FUNCTION divide_floats(a FLOAT, b FLOAT) 
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+    DECLARE result FLOAT;
+    IF b = 0 THEN
+        RETURN NULL; -- уникнення ділення на 0
+    END IF;
+    SET result = a / b;
+    RETURN result;
+END//
+
+DELIMITER ;
